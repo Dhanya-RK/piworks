5fa5c15b9b113919358e7332231afb7953762b76
dramakrishnan6 <dramakrishnan6@gatech.edu>
2021-10-22 12:33:22 -0400

    Improved the user messages

--- SwapClever.java
+++ SwapClever.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //Swap the values of two variables without using a third variable.
 public class SwapClever {
 	
@@ -6,11 +8,22 @@ public class SwapClever {
 	}
 	
 	public static void main(String[] args) {
-		int a = 1;
-		int b = 2;
-		System.out.println("Before : a = " + a + ", b = " +  b);
-		a = swap(b, b=a);		//A copy of the parameters are created internally.
-		System.out.println("After : a = " + a + ", b = " +  b);	
+		Scanner sc = new Scanner(System.in);  
+		try {			
+			System.out.print("Please give a number as 1st variable: ");  
+			int a = sc.nextInt();  
+			System.out.print("Please give a number as 2nd variable: ");  
+			int b = sc.nextInt();  		 
+			System.out.println("\nYou gave: first variable = " + a + ", second variable = " +  b);
+			
+			a = swap(b, b = a);	//A copy of the function parameters are stored internally in the method stack, hence the value of b is not lost.
+			
+			System.out.println("\nResult:\nThe value of the 1st variable: " + a);
+			System.out.println("The value of the 2nd variable: " + b);	
+		} catch (java.util.InputMismatchException e) {
+			System.out.println("Please input a valid number.Exiting..");
+            System.exit(0);
+        }
 	}
 
 }
